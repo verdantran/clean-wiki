13635914b57e7da579f6b5c0ff1ccf102e614e1a
James Burnham <labpd01@googlemail.com>
2019-10-23 23:19:03 +0100

    Removed default components and added new article component

---
+++ src/components/newArticle/NewArticle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <TextEditor />
+  </div>
+</template>
+
+<script>
+import TextEditor from '../common/TextEditor.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TextEditor
+  }
+}
+</script>
