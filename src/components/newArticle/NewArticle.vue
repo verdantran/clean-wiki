cde5765e96eb18b69cb0c5bfafcb56719c6f3af2
James Burnham <labpd01@googlemail.com>
2019-10-24 20:21:14 +0100

    Got editor styles in place

--- src/components/newArticle/NewArticle.vue
+++ src/components/newArticle/NewArticle.vue
@@ -1,16 +1,50 @@
 <template>
   <div id="app">
-    <TextEditor />
+    <div class="editor-container">
+      <div class="article-title-container">
+        <medium-editor
+          data-placeholder="Title"
+          :text="articleTitle"
+          v-on:edit="articleTitleEdit"
+        />
+      </div>
+      <medium-editor
+        data-placeholder="Article content"
+        :text="articleText"
+        v-on:edit="articleTextEdit"
+      />
+    </div>
+    <!-- {{articleTitle}}<br/>
+    {{articleText}} -->
   </div>
 </template>
 
 <script>
-import TextEditor from '../common/TextEditor.vue'
+import editor from 'vue2-medium-editor'
 
 export default {
   name: 'app',
   components: {
-    TextEditor
+    'medium-editor': editor
+  },
+  data: function () {
+    return {
+      articleText: '',
+      articleTitle: ''
+    }
+  },
+  methods: {
+    articleTitleEdit: function (operation) {
+      this.articleTitle = operation.api.origElements.innerHTML
+    },
+    articleTextEdit: function (operation) {
+      this.articleText = operation.api.origElements.innerHTML
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => this.save(), 2000)
+    },
+    save: function () {
+      console.log(`saving`)
+    }
   }
 }
 </script>
